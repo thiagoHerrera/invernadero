59fa4fb96034166d9c72853a91b378323dbb89e3
bautInsa07 <bautiimateo07@gmail.com>
2025-05-14 00:23:38 -0300

    landing ya termina, con todas sus funcionalidades

--- Invernadero/Windows/urls.py
+++ Invernadero/Windows/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import landing, home
+from .views import landing, home, equipo, contacto, funciones, info
 
 
 urlpatterns = [
     path('', landing, name="landing"),
-    path('home/', home, name="home")
+    path('home/', home, name="home"),
+    path('equipos/', equipo, name="equipos"),
+    path('contacto/', contacto, name="contacto"),
+    path('funciones/', funciones, name="funciones"),
+    path('informacion/', info, name="info")
+
 ]
--- Invernadero/Windows/views.py
+++ Invernadero/Windows/views.py
@@ -5,4 +5,16 @@ def home(request):
 
 def landing(request):
     return render(request, 'landing.html')
+
+def equipo(request):
+    return render(request, 'equipos.html')
+
+def contacto(request):
+    return render(request, 'contacto.html')
+
+def funciones(request):
+    return render(request, 'funciones.html')
+
+def info(request):
+    return render(request, 'info.html')
 # Create your views here.
