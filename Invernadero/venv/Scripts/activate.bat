ec057ba8bd0c1f09f4a3cec30b99c1a87fd0dfa8
thiagoHerrera <tititomash@gmail.com>
2025-08-19 18:36:21 -0300

    ahora anda, (corregi las versiones de las dependencias)

--- Invernadero/venv/Scripts/activate.bat
+++ Invernadero/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\alumnos\Desktop\repo-git\invernadero\invernadero\venv
+set VIRTUAL_ENV=C:\Users\Alumnos\Desktop\invernadero\Invernadero\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
