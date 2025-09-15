31505bc2750489ad4a68cfc77b123cf30cb22035
Manu-Gonzalez <manuadangonzales@gmail.com>
2025-09-15 22:37:04 +0000

    solucion de variables de entornos

--- Invernadero/invernadero/settings.py
+++ Invernadero/invernadero/settings.py
@@ -5,7 +5,11 @@ This file imports settings based on the environment.
 """
 
 import os
-
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    ".onrender.com",  # permite cualquier subdominio de Render
+]
 # Determine which settings to use
 ENVIRONMENT = os.getenv('DJANGO_ENV', 'development')
 
