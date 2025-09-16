afc997ad7b9cc2d5641c5316c619cd63015c93b6
Manu-Gonzalez <manuadangonzales@gmail.com>
2025-09-16 23:13:17 +0000

    se reformo el modulo de invernadero

--- Invernadero/invernadero/urls.py
+++ Invernadero/invernadero/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from users import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,5 +11,5 @@ urlpatterns = [
     path('Verificion2FA/', include('Verificacion2FA.urls')),
     path('ResetPassword/', include('Authentication.urls')),
     path('', include('Windows.urls')),
-     path('diagnostico/', include('diagnostico.urls')),
-]
+    path('diagnostico/', include('diagnostico.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
